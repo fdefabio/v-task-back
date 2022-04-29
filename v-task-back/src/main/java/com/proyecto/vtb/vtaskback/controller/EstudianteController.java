c7fd34864bfd190d4f96450122ae6dae9f86a066
fabio <fabioannypro47@gmail.com>
2022-04-29 17:01:15 -0500

    api para coleccion lista

--- v-task-back/src/main/java/com/proyecto/vtb/vtaskback/controller/EstudianteController.java
+++ v-task-back/src/main/java/com/proyecto/vtb/vtaskback/controller/EstudianteController.java
@@ -1,4 +1,48 @@
 package com.proyecto.vtb.vtaskback.controller;
 
+import com.proyecto.vtb.vtaskback.Service.api.EstudianteServiceAPI;
+import com.proyecto.vtb.vtaskback.estudiante.model.Estudiante;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
 public class EstudianteController {
+
+    @Autowired
+    private EstudianteServiceAPI estudianteServiceAPI;
+
+    @GetMapping("/Estudiantes")
+    public List<Estudiante> getEstudiantes(){
+         return estudianteServiceAPI.getAll();
+    }
+
+    @GetMapping("/Estudiante/{id}")
+    public Estudiante buscarEstudiante(@PathVariable Long id){
+        return estudianteServiceAPI.get(id);
+    }
+
+
+    @PostMapping("/AgrgarEstudiante")
+    public ResponseEntity <Estudiante> agregarEstudiante(@RequestBody Estudiante estudiante){
+        Estudiante obj = estudianteServiceAPI.save(estudiante);
+        return new ResponseEntity<Estudiante>(obj , HttpStatus.OK);
+    }
+
+    @DeleteMapping("/EliminarEstudiante/{id}")
+    public ResponseEntity<Estudiante>eliminarEstudiante(@PathVariable Long id){
+        Estudiante estudiante = estudianteServiceAPI.get(id);
+        if (estudiante != null){
+            estudianteServiceAPI.delete(id);
+        }else{
+            return new ResponseEntity<Estudiante>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<Estudiante>(estudiante , HttpStatus.OK);
+    }
 }
